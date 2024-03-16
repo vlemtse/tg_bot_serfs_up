44d3909e5b04aba1a96b4d921003571a722b4e56
Lemtse Vitaliy <lemtsevv@gmail.com>
2024-03-17 02:23:36 +0300

    init

--- update.sh
+++ update.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-
+echo "Starting update"
 git pull origin master
+echo "Git pull complete"
 
 sudo systemctl stop bot_su.service
+echo "Stopping bot_su.service"
 
 pip install -r requirements.txt
+echo "Requirements installed"
 
 alembic upgrade head
+echo "Alembic upgrade completed"
 
 sudo systemctl start bot_su.service
+echo "Started bot_su.service"
+
+echo "Update complete"
